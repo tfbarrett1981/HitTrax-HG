d4f048c518903bfce440c349728c969097aa12e0
tfbarrett1981 <tfbarrett@gmail.com>
2015-11-16 16:20:04 -0500

    Gulp Less

---
+++ .vscode/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function() {
+    gulp.src('css/*.less')
+        .pipe(less())
+        .pipe(gulp.dest(function(f) {
+            return f.base;
+        }))
+});
+
+gulp.task('default', function() {
+    gulp.watch('css/*.less', ['less']);
+})
