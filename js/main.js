7a848db9c4daa579c06706a2b8d5bdf34646c0b7
tfbarrett1981 <tfbarrett@gmail.com>
2015-11-17 15:43:31 -0500

    Reset Actions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 $( document ).ready(function() {
-	$(".buttonBorder").click(function(e){
+	$(".forgot-password-template .buttonBorder").click(function(e){
 		$(this).hide();
 		var items = Array("foundAccount", "noAccount");
 		
 		var item = items[Math.floor(Math.random()*items.length)];
+		$("#" + item).removeClass("hidden");
+	});
+	
+	$(".reset-password-template .buttonBorder").click(function(e){
+		
+		var items = Array("resetSuccess", "resetFail");
+		
+		var item = items[Math.floor(Math.random()*items.length)];
+		if(item == "resetSuccess")
+		{
+			$(".passwordText, .passwordInputBox").addClass("hidden");
+		}
+		
+		if(item == "resetFail")
+		{
+			$(".passwordText").addClass("hidden");
+		}
 		$("#" + item).removeClass("hidden");
 	});
     
